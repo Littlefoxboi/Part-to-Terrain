197430c6093bda63e19713670cee1b622555d94c
Fastcar48 <mkargus48@gmail.com>
2018-03-22 20:44:35 -0400

    V3.0.2
    https://fastcar48.github.io/parttoterrain/changes#V3.0.2

--- Plugin.lua
+++ Plugin.lua
@@ -1,7 +1,7 @@
 -- Made By Fasty48
 local Button = plugin:CreateToolbar('Fastcar48'):CreateButton('Part to Terrain','','rbxassetid://297321964')
 local Mouse = plugin:GetMouse()
-local Version = '3.0.1'
+local Version = '3.0.2'
 
 local Color={
 	Black = Color3.fromRGB(33,33,33),
@@ -42,7 +42,7 @@ SelectionSphere.Color3 = Color.Blue
 local MainFrame = Instance.new('Frame',UI)
 MainFrame.Active = true
 MainFrame.BackgroundColor3 = Color.Black
-MainFrame.BackgroundTransparency = .25
+MainFrame.BackgroundTransparency = .5
 MainFrame.BorderSizePixel = 0
 MainFrame.Position = UDim2.new(0,5,0,5)
 MainFrame.Size = UDim2.new(0,185,0,270)
@@ -55,7 +55,7 @@ header.Size = UDim2.new(1,0,0,5)
 
 local Title = Instance.new('TextLabel',MainFrame)
 Title.BackgroundColor3 = Color.Black
-Title.BackgroundTransparency = .6
+Title.BackgroundTransparency = .5
 Title.BorderSizePixel = 0
 Title.Position = UDim2.new(0,5,0,10)
 Title.Size = UDim2.new(1,-10,0,20)
@@ -66,18 +66,18 @@ Title.TextSize = 15
 
 local HoverMaterial = Instance.new('TextLabel',MainFrame)
 HoverMaterial.BackgroundColor3 = Color.Black
-HoverMaterial.BackgroundTransparency = .6
+HoverMaterial.BackgroundTransparency = .5
 HoverMaterial.BorderSizePixel = 0
 HoverMaterial.Position = UDim2.new(0,5,0,245)
 HoverMaterial.Size = UDim2.new(1,-10,0,20)
 HoverMaterial.Font = Enum.Font.SourceSans
 HoverMaterial.Text = ''
 HoverMaterial.TextColor3 = Color.White
-HoverMaterial.TextSize = 14
+HoverMaterial.TextSize = 15
 
 local PaddingFrame = Instance.new('Frame',MainFrame)
 PaddingFrame.BackgroundColor3 = Color.Black
-PaddingFrame.BackgroundTransparency = .6
+PaddingFrame.BackgroundTransparency = .5
 PaddingFrame.BorderSizePixel = 0
 PaddingFrame.Position = UDim2.new(0,5,0,35)
 PaddingFrame.Size = UDim2.new(0,175,0,205)
@@ -88,11 +88,11 @@ SelectionFrame.BackgroundTransparency = 1
 SelectionFrame.BorderSizePixel = 0
 SelectionFrame.Position = UDim2.new(0,5,0,5)
 SelectionFrame.Size = UDim2.new(0,165,0,195)
-SelectionFrame.BottomImage = 'rbxassetid://285089596'
+SelectionFrame.BottomImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
 SelectionFrame.CanvasSize = UDim2.new(0,0,1.15,0)
-SelectionFrame.MidImage = 'rbxassetid://285089596'
+SelectionFrame.MidImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
 SelectionFrame.ScrollBarThickness = 5
-SelectionFrame.TopImage = 'rbxassetid://285089596'
+SelectionFrame.TopImage = 'rbxasset://textures/ui/Scroll/scroll-middle.png'
 
 local GridLayout = Instance.new('UIGridLayout',SelectionFrame)
 GridLayout.CellPadding = UDim2.new(0,5,0,5)
@@ -120,32 +120,30 @@ end
 
 function AddTerrain(Part)
 	local Material = Enum.Material[Materials[Settings.MaterialSelected]]
-	if Part then
-		if Part:IsA('Part') then
-			if Part.Shape == Enum.PartType.Block then
-				workspace.Terrain:FillBlock(Part.CFrame,Part.Size,Material)
-				Part:Remove()
-				Services.ChangeHistory:SetWaypoint('PTT')
-			elseif Part.Shape == Enum.PartType.Ball then
-				workspace.Terrain:FillBall(Part.Position,Part.Size.X/2,Material)
-				Part:Remove()
-				Services.ChangeHistory:SetWaypoint('PTT')
-			else
-				CreateErrorMsg('Shape is not supported.')
-			end
+	if Part and Part:IsA('Part') then
+		if Part.Shape == Enum.PartType.Block then
+			workspace.Terrain:FillBlock(Part.CFrame,Part.Size,Material)
+			Part:Remove()
+			Services.ChangeHistory:SetWaypoint('PTT')
+		elseif Part.Shape == Enum.PartType.Ball then
+			workspace.Terrain:FillBall(Part.Position,Part.Size.X/2,Material)
+			Part:Remove()
+			Services.ChangeHistory:SetWaypoint('PTT')
 		else
-			CreateErrorMsg('Part Type is not supported.')
+			CreateErrorMsg('Shape is not supported.')
 		end
+	else
+		CreateErrorMsg('Part Type is not supported.')
 	end
 end
 
 function CreateErrorMsg(reason)
 	local Error_MainFrame = Instance.new('Frame',UI)
 	Error_MainFrame.BackgroundColor3 = Color.Black
-	Error_MainFrame.BackgroundTransparency = .25
+	Error_MainFrame.BackgroundTransparency = .5
 	Error_MainFrame.BorderSizePixel = 0
-	Error_MainFrame.Position = UDim2.new(.5,-150,.8,-100)
-	Error_MainFrame.Size = UDim2.new(0,300,0,100)
+	Error_MainFrame.Position = UDim2.new(.5,-150,.9,-100)
+	Error_MainFrame.Size = UDim2.new(0,300,0,90)
 
 	local header_b = header:Clone()
 	header_b.Parent = Error_MainFrame
@@ -158,35 +156,38 @@ function CreateErrorMsg(reason)
 	Error_Title.Font = Enum.Font.SourceSansBold
 	Error_Title.Text = 'Part to Terrain Error'
 	Error_Title.TextColor3 = Color.White
-	Error_Title.TextSize = 18
+	Error_Title.TextSize = 15
 
 	local Reason = Instance.new('TextLabel',Error_MainFrame)
 	Reason.BackgroundTransparency = 1
 	Reason.Position = UDim2.new(0,0,0,40)
-	Reason.Size = UDim2.new(1,0,0,55)
+	Reason.Size = UDim2.new(1,0,0,40)
 	Reason.Font = Enum.Font.SourceSans
 	Reason.Text = reason
 	Reason.TextColor3 = Color.White
-	Reason.TextSize = 18
+	Reason.TextSize = 15
 	Reason.TextWrapped = true
 
 	game.Debris:AddItem(Error_MainFrame,5)
 end
 
-function CreateImgBtn(Name,ID,Material)
+function CreateImgBtn(Name,ID,Enum)
 	local Btn = Instance.new('ImageButton',SelectionFrame)
 	Btn.BorderSizePixel = 0
 	Btn.Image = 'rbxassetid://'..ID
 	Btn.Name = Name
 	table.insert(UIButtons,Btn)
-	if not Material then
+	if not Enum then
 		table.insert(Materials,Name)
 	else
-		table.insert(Materials,Material)
+		table.insert(Materials,Enum)
 	end
 	Btn.LayoutOrder = #UIButtons
-	Btn.MouseButton1Click:connect(function() UISelect(Btn.LayoutOrder) end)
-	Btn.MouseEnter:connect(function() HoverMaterial.Text = Btn.Name end)
+	Btn.MouseButton1Click:connect(function()
+		Settings.MaterialSelected = Btn.LayoutOrder
+		SelectedImg.Parent = UIButtons[Btn.LayoutOrder]
+	end)
+	Btn.MouseEnter:connect(function() HoverMaterial.Text = Name end)
 	Btn.MouseLeave:connect(function() HoverMaterial.Text = '' end)
 	return Btn
 end
@@ -194,7 +195,7 @@ end
 function CreateUpdateMsg()
 	local Update_MainFrame = Instance.new('Frame',UI)
 	Update_MainFrame.BackgroundColor3 = Color.Black
-	Update_MainFrame.BackgroundTransparency = .25
+	Update_MainFrame.BackgroundTransparency = .5
 	Update_MainFrame.BorderSizePixel = 0
 	Update_MainFrame.Position = UDim2.new(1,-200,0.8,0)
 	Update_MainFrame.Size = UDim2.new(0,200,0,75)
@@ -219,12 +220,12 @@ function CreateUpdateMsg()
 	Update_Desc.Font = Enum.Font.SourceSans
 	Update_Desc.Text = 'The current Part to Terrain version you are running is outdated.'
 	Update_Desc.TextColor3 = Color.White
-	Update_Desc.TextSize = 14
+	Update_Desc.TextSize = 15
 	Update_Desc.TextWrapped = true
 
 	local Update_Btn = Instance.new('TextButton',Update_MainFrame)
 	Update_Btn.BackgroundColor3 = Color.Black
-	Update_Btn.BackgroundTransparency = .25
+	Update_Btn.BackgroundTransparency = .5
 	Update_Btn.BorderSizePixel = 0
 	Update_Btn.Position = UDim2.new(0,-30,0,0)
 	Update_Btn.Size = UDim2.new(0,30,0,30)
@@ -232,18 +233,13 @@ function CreateUpdateMsg()
 	Update_Btn.Text = 'X'
 	Update_Btn.TextColor3 = Color.White
 	Update_Btn.TextSize = 20
-
 	Update_Btn.MouseButton1Click:connect(function() Update_MainFrame:Remove() end)
-end
-
-function UISelect(number)
-	Settings.MaterialSelected = number
-	SelectedImg.Parent = UIButtons[number]
 end
 
 --------------------
 --Material List
 --------------------
+CreateImgBtn('Air',765103830)
 CreateImgBtn('Asphalt',397352644)
 CreateImgBtn('Basalt',254542066)
 CreateImgBtn('Brick',225315419)
@@ -273,15 +269,13 @@ CreateImgBtn('Wooden Planks',225315705,'WoodPlanks')
 Mouse.Move:connect(function()
 	local Part = Mouse.Target
 	if Settings.PluginEnabled then
-		if Part then
-			if Part:IsA('Part') then
-				if Part.Shape == Enum.PartType.Block then
-					SelectionBox.Adornee = Part
-					SelectionSphere.Adornee = nil
-				elseif Part.Shape == Enum.PartType.Ball then
-					SelectionBox.Adornee = nil
-					SelectionSphere.Adornee = Part
-				end
+		if Part and Part:IsA('Part') then
+			if Part.Shape == Enum.PartType.Block then
+				SelectionBox.Adornee = Part
+				SelectionSphere.Adornee = nil
+			elseif Part.Shape == Enum.PartType.Ball then
+				SelectionBox.Adornee = nil
+				SelectionSphere.Adornee = Part
 			end
 		else
 			SelectionBox.Adornee = nil
@@ -301,11 +295,7 @@ end)
 --------------------
 Button.Click:connect(function()
 	if not Services.Run:IsRunning() then
-		if Settings.PluginEnabled then
-			Activate(false)
-		else
-			Activate(true)
-		end
+		Activate(not Settings.PluginEnabled)
 	else
 		CreateErrorMsg("Part to Terrain will not work while on 'Play Solo' or 'Play'.")
 	end
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Part to Terrain is a plugin for the Roblox engine that allows to converts parts into terrain with ease.
 
-[Download on Roblox](https://www.roblox.com/library/261634767/Part-to-Terrain) | [Source Code](https://github.com/Fastcar48/Part-to-Terrain/blob/master/Plugin.lua) | [Change logs](https://fastcar48.github.io/parttoterrain/changes)
+[Download on Roblox](https://roblox.com/library/261634767/Part-to-Terrain) | [Source Code](https://github.com/Fastcar48/Part-to-Terrain/blob/master/Plugin.lua) | [Change logs](https://fastcar48.github.io/parttoterrain/changes) | [Report a bug](https://github.com/Fastcar48/Part-to-Terrain/issues/new)
 
 ## Reviews
 "This is the most handy plugin! I have it because I need to overlay the old cube terrain. AWESOME!" -maxadler1234
@@ -22,14 +22,14 @@ Part to Terrain is a plugin for the Roblox engine that allows to converts parts 
 
 **Q: I found a bug/glitch/issue with Part to Terrain. How do I report it?**
 
-**A:** Send a tweet to [@Fasty48_Dev](https://twitter.com/Fasty48_Dev) or use the [Issue List](https://github.com/Fastcar48/Part-to-Terrain/issues). Please be sure to provide what version is having this bug and the details of the error.
+**A:** Send a tweet to [@Fasty48](https://twitter.com/Fasty48) or use the [Issue List](https://github.com/Fastcar48/Part-to-Terrain/issues). Please be sure to provide what version is having this bug and the details of the error.
 
 **Q: Can I make a edited version of Part to Terrain?**
 
 **A:** Yes, but please do give credit if you upload it to Roblox.
 
 ## Credits
-- [TigerCaptain](https://www.roblox.com/users/19053090/profile) - For the idea with his Part-To-Terrain
-- [CloneTrooper1019](https://www.roblox.com/users/2032622/profile/) - Helping on the orignal Part to Terrain
+- [TigerCaptain](https://roblox.com/users/19053090/profile) - For the idea with his Part-To-Terrain
+- [CloneTrooper1019](https://roblox.com/users/2032622/profile) - Helping on the orignal Part to Terrain
 - [Valletta](https://twitter.com/valletta__) - Created the logo
 - Everyone that downloaded Part to Terrain on Roblox
